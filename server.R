39931fa98329bb2351faeb3f2f1302901d52a246
dimtriant <trjim2k5@gmail.com>
2015-04-26 14:25:51 +0100

    Added Backoff

--- server.R
+++ server.R
@@ -19,12 +19,30 @@ predict_fun <- function(inputString, unigram.df, bigram.df, trigram.df, ngram_to
       prediction1 <- prediction.df$predicted[1]
       prediction2 <- prediction.df$predicted[2]
       prediction3 <- prediction.df$predicted[3]
+      
+      if (is.na(prediction1)) {
+        prediction.df <- bigram.df[(bigram.df$word1==inputString.token[2]),]
+        row.names(prediction.df) <- NULL
+        prediction1 <- prediction.df$predicted[1]
+        prediction2 <- prediction.df$predicted[2]
+        prediction3 <- prediction.df$predicted[3]
+      }
+      
     } else if (length(inputString.token) == 2) {
       prediction.df <- trigram.df[(trigram.df$word1==inputString.token[1] & trigram.df$word2==inputString.token[2]),]
       row.names(prediction.df) <- NULL
       prediction1 <- prediction.df$predicted[1]
       prediction2 <- prediction.df$predicted[2]
       prediction3 <- prediction.df$predicted[3]
+      
+      if (is.na(prediction1)) {
+        prediction.df <- bigram.df[(bigram.df$word1==inputString.token[2]),]
+        row.names(prediction.df) <- NULL
+        prediction1 <- prediction.df$predicted[1]
+        prediction2 <- prediction.df$predicted[2]
+        prediction3 <- prediction.df$predicted[3]
+      }
+      
     } else if (length(inputString.token) == 1) {
       prediction.df <- bigram.df[(bigram.df$word1==inputString.token[1]),]
       row.names(prediction.df) <- NULL
